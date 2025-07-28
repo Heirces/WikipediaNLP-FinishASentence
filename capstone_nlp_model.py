0baa1ae1c44f6a0718c046463f0eb6d8fc3adba5
Heirces <Heirces@gmail.com>
2025-07-28 00:35:55 -0700

    Version right before testing model with 1GB file

--- capstone_nlp_model.py
+++ capstone_nlp_model.py
@@ -1,21 +1,27 @@
 import tensorflow as tf
-print("TF Version:", tf.__version__)
-print("Has Keras:", hasattr(tf, 'keras'))
+from tensorflow.keras.preprocessing.text import Tokenizer
+import numpy as np
 
 
 
-class Capstone_Tokenization:
+class Capstone_NLP_Model:
     def __init__(self):
         self.MIN_ALLOWED_TOKENS_IN_A_SENTENCE = 6
         self.MAX_ALLOWED_TOKENS_IN_A_SENTENCE = 30
+        self.OUTPUT_DIMENSION_SIZE_64 = 64
         self.avg_sentence_length_in_tokens = None
         self.total_sentences_being_trained_on = 0
 
         self.total_used_sentences_in_text_sentences = 0
         self.total_tokens_in_used_sentences = 0
+        self.avg_tokens_in_a_sentence = 0
 
         self.list_of_arrays_of_tokenized_sentences = [] 
-        self.list_of_training_arrays_by_sentence = []  
+        self.list_of_training_arrays_strings = []  
+        self.list_of_training_arrays_ids = []
+        self.list_of_training_inputs = []
+        self.list_of_training_labels = []
+        self.model = None
     
     def create_list_of_tokenized_sentences(self, doc):
         text_sentences = list(doc.sents)
@@ -32,32 +38,25 @@ class Capstone_Tokenization:
                     list_of_tokens_building_into_a_sentence.append(token.text)
             self.list_of_arrays_of_tokenized_sentences.append(list_of_tokens_building_into_a_sentence)
         
-        avg_tokens_in_sentence = self.total_tokens_in_used_sentences // self.total_used_sentences_in_text_sentences
+        self.avg_tokens_in_a_sentence = self.total_tokens_in_used_sentences // self.total_used_sentences_in_text_sentences
         
-        #print(f"Average: {avg_tokens_in_sentence}\nNum Sentences: {self.total_used_sentences_in_text_sentences}\nTokenized Sents: {self.list_of_arrays_of_tokenized_sentences}")
-
-        return {
-        "avg": avg_tokens_in_sentence,
-        "num_sentences": self.total_used_sentences_in_text_sentences,
-        "tokenized_sentences": self.list_of_arrays_of_tokenized_sentences
-    }
 
     def create_list_of_sentence_training_arrays(self):
         for tokenized_sentence in self.list_of_arrays_of_tokenized_sentences:
             sentence_length = len(tokenized_sentence)
             half_sentence_length = sentence_length // 2
-            training_sentences = []
+            training_sentence = []
 
             if sentence_length > 24:
                 step = 3
-            elif sentence_length >= 15 or sentence_length <= 24:
+            elif sentence_length >= 15 and sentence_length <= 24:
                 step = 2
             else:
                 step = 1
 
             for token in range(half_sentence_length, sentence_length + 1, step):
-                training_sentences.append(tokenized_sentence[:token])
-            self.list_of_training_arrays_by_sentence.append(training_sentences)
+                training_sentence = tokenized_sentence[:token]
+                self.list_of_training_arrays_strings.append(training_sentence)
 
             
             
--- clean_articles.py
+++ clean_articles.py
@@ -9,37 +9,47 @@ pattern_carrot_text = r"\<.*?\>"
 pattern_titles = r"={2,6}[^=].*?[^=]={2,6}"
 pattern_Categories_at_end_of_articles = r"Category:.*"
 pattern_URLs = r"(https?://)?([a-zA-Z0-9\-]+\.)+[a-z]{2,6}(/[^\s]*)?"
-
-with zipfile.ZipFile("archive.zip", 'r') as wikipedia_archive:
-    clean_text_file_name_base = "cleaned_text_files/clean_text"     # Every 30 cleaned json files will be appended to a single clean_text file
-    clean_text_file_number = 1 
-    number_of_json_files = 0
-    for file in wikipedia_archive.namelist():
-        if file.endswith('.json'):
-            number_of_json_files += 1
-            if number_of_json_files % 30 == 0:
-                clean_text_file_number += 1
-            if number_of_json_files % 10 == 0:
-                print(f"Processed {number_of_json_files} JSON files...")
-            clean_text_file_name = clean_text_file_name_base + str(clean_text_file_number) + ".txt"
-            with wikipedia_archive.open(file) as json_file:
-                try:
-                    data = json.load(json_file)
-                except json.JSONDecodeError:
-                    print(f"Skipping corrupted json file: {file}")
-                    continue
-                for article in data:
-                    article_text = article["text"]
-                    clean_text = re.sub(pattern_table, '', article_text)
-                    clean_text = re.sub(pattern_cleanup_table_leftovers, '', clean_text)
-                    clean_text = re.sub(pattern_table_cell_cleanup, '', clean_text)
-                    clean_text = re.sub(pattern_carrot_text, '', clean_text)
-                    clean_text = re.sub(pattern_titles, '', clean_text)
-                    clean_text = re.sub(pattern_Categories_at_end_of_articles, "", clean_text, flags=re.DOTALL)
-                    clean_text = re.sub(r"\*", '', clean_text)
-                    clean_text = re.sub(pattern_URLs, '', clean_text)
-                    
-                    with open(clean_text_file_name, "a", encoding="utf-8") as out_file:
-                        out_file.write(clean_text.strip() + "\n\n")
+pattern_quotes = r'["“”‘’\'`]'
+def create_clean_text_files_from_archive_zip():
+    with zipfile.ZipFile("archive.zip", 'r') as wikipedia_archive:
+        clean_text_file_name_base = "cleaned_text_files/clean_text"     # Every 30 cleaned json files will be appended to a single clean_text file
+        clean_text_file_number = 1 
+        number_of_json_files = 0
+        for file in wikipedia_archive.namelist():
+            if file.endswith('.json'):
+                number_of_json_files += 1
+                if number_of_json_files % 30 == 0:
+                    clean_text_file_number += 1
+                if number_of_json_files % 10 == 0:
+                    print(f"Processed {number_of_json_files} JSON files...")
+                clean_text_file_name = clean_text_file_name_base + str(clean_text_file_number) + ".txt"
+                with wikipedia_archive.open(file) as json_file:
+                    try:
+                        data = json.load(json_file)
+                    except json.JSONDecodeError:
+                        print(f"Skipping corrupted json file: {file}")
+                        continue
+                    for article in data:
+                        article_text = article["text"]
+                        clean_text = re.sub(pattern_table, '', article_text)
+                        clean_text = re.sub(pattern_cleanup_table_leftovers, '', clean_text)
+                        clean_text = re.sub(pattern_table_cell_cleanup, '', clean_text)
+                        clean_text = re.sub(pattern_carrot_text, '', clean_text)
+                        clean_text = re.sub(pattern_titles, '', clean_text)
+                        clean_text = re.sub(pattern_Categories_at_end_of_articles, "", clean_text, flags=re.DOTALL)
+                        clean_text = re.sub(r"\*", '', clean_text)
+                        clean_text = re.sub(pattern_URLs, '', clean_text)
+                        clean_text = re.sub(pattern_quotes, '', clean_text)
+                        
+                        with open(clean_text_file_name, "a", encoding="utf-8") as out_file:
+                            out_file.write(clean_text.strip() + "\n\n")
     
-                
+def reclean_the_clean_text_files(clean_text_file):
+    with open(clean_text_file, "r", encoding="utf-8") as working_file:
+        text = working_file.read()
+    cleaned_text = re.sub(pattern_quotes, '', text)
+    print("Halfway Done")
+    with open(clean_text_file, "w", encoding="utf-8") as final_file:
+        final_file.write(cleaned_text)
+
+reclean_the_clean_text_files("cleaned_text_files\clean_text1.txt")
